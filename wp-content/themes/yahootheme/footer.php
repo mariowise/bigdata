82740ce1bf747de0667f32c822200ff0ff306dc1
Mario López <mariolopezlandes@gmail.com>
2013-09-14 01:22:23 -0300

    Footer (80%)

---
+++ wp-content/themes/yahootheme/footer.php
@@ -0,0 +1,49 @@
+
+		<div id="wrapper-ghost"></div>
+
+		<footer>
+
+			<div id="footer-big">
+				<div class="row">
+					
+					<div class="large-2 columns">
+						<img style="width: 100%" src="<?php bloginfo('template_directory');?>/img/logoUsach.png">
+					</div>
+					<div class="large-4 columns">
+						<div>Enlaces</div>
+						<div>
+							<ul>
+								<li><a href="http://www.udesantiago.cl/">Universidad de Santiago de Chile</a></li>
+								<li><a href="http://www.udesantiagovirtual.cl/">UsachVirtual</a></li>
+								<li><a href="http://informatica.usach.cl/">Departamento de ingeniería informática</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="large-4 columns">
+						<div>Comparte</div>
+						<div>
+							<div class="social-link"><a href="#"><i class="icon-facebook"></i></a></div>
+							<div class="social-link"><a href="#"><i class="icon-twitter"></i></a></div>
+							<div class="social-link"><a href="#"><i class="icon-google-plus"></i></a></div>
+						</div>
+						<div>Contacto</div>
+					</div>
+					<div class="large-2 columns">
+						<div>Miembros</div>
+					</div>
+
+				</div>
+			</div>	
+			<div id="footer-small">
+				<div class="row">
+					<div>
+						Sitio web construido con wordpress por <a href="http://mariowise.wordpress.com/">@mariowise</a>
+					</div>
+				</div>
+			</div>
+
+		</footer>
+
+	</div><!-- wrapper -->
+
+</body>
