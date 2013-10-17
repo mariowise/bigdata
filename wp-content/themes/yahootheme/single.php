7f2e266b67c30f502e416a652b64304434c44bc6
Mario López <mariolopezlandes@gmail.com>
2013-10-17 19:57:16 -0300

    Fuentes título gris

--- wp-content/themes/yahootheme/single.php
+++ wp-content/themes/yahootheme/single.php
@@ -3,18 +3,28 @@
 
 	<?php the_post() ?>
 
-	<div class="row">
+	<div id="main" class="row">
 
 		<div class="columns small-12">
-			<div></div>
-			<div>
-				<?php the_title('<h2>','</h2>');?>
-				<?php the_time('F m, Y');?>
-				<?php the_post_thumbnail('wpf-featured'); ?>
-				<?php the_content();?>
+			<div class="post">
+				<div class="row">
+					<div class="columns small-1">
+						<div class="date" style="float: left">
+							<div><?php the_time('j');?></div>
+							<div><?php the_time('m');?>-<?php the_time('Y');?></div>
+						</div>
+					</div>
+					<div class="columns small-11 title"><?php the_title('<h2>','</h2>');?></div>
+				</div>
+				<div id="post-content" class="row">
+					<div class="columns small-4">
+						<?php the_post_thumbnail('wpf-featured'); ?>
+					</div>
+					<div class="columns small-8"><?php the_content();?></div>
+				</div>
 			</div>
 			<hr>
-			<div class="row">
+			<div class="row post-link-move">
 				<div class="columns large-6 small-6">
 					<?php previous_post_link('<i class="icon-double-angle-left"></i> %link'); ?> 
 				</div>
